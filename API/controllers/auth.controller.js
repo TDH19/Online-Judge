feeedeb1d2b01d8462079618ef98057c02c03035
Taral Munot <taralm0410@gmail.com>
2025-07-13 12:53:40 +0530

    Created Sign-In api route

--- API/controllers/auth.controller.js
+++ API/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import errorHandler from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 
 export const signup = async (req,res,next) =>{
@@ -20,3 +22,17 @@ export const signup = async (req,res,next) =>{
     }
 };
 
+export const signin = async (req,res,next) =>{
+    const {username,password} = req.body;
+    try {
+        const validUser = await User.findOne({ username });
+        if (!validUser) next(errorHandler(404, "User not found!"));
+        const isPasswordValid = bcrypt.compareSync(password, validUser.password);
+        if (!isPasswordValid) next(errorHandler(401, "Invalid password!"));
+        const token = jwt.sign({id : validUser._id}, process.env.JWT_SECRET);
+        const {password:pass, ...rest} = validUser._doc; // Exclude password from response
+        res.cookie("access_token", token, {httpOnly: true, secure: true, expires: new Date(Date.now() + 24*60*60*1000)}).status(200).json(rest); // 1 hour expiration
+    } catch (error) {
+        next(error);
+    }
+};
